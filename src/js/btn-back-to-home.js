0c6a5044efc029561ecd2fe2f03c068d52df5f8b
Yuraro <romanykyriy@gmail.com>
2023-01-15 21:06:33 -0800

    Update

--- src/js/btn-back-to-home.js
+++ src/js/btn-back-to-home.js
@@ -1,6 +1,15 @@
-document.querySelector(".back-home-btn").addEventListener("click", function () {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+const backHomeBtn = document.getElementById("back-home");
+const firstSection = document.getElementById("first-section");
+const firstSectionPosition = firstSection.offsetTop;
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > firstSectionPosition) {
+        backHomeBtn.classList.remove("hidden");
+    } else {
+        backHomeBtn.classList.add("hidden");
+    }
+});
+
+backHomeBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
 });
