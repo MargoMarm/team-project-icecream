5649666953e822bfba7896a1569b07a5fa6274ef
MargoMarm <margogerasimenko@gmail.com>
2023-01-16 18:21:22 +0100

    prevent body scroll

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,27 +2,30 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const body = document.querySelector('body');
 
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
+	  mobileMenu.classList.toggle('is-open');
+	  body.classList.toggle('no-scroll');
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
+	closeMenuBtn.addEventListener('click', toggleMenu);
+	 
+  
+  
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
+         
+
   });
 })();
